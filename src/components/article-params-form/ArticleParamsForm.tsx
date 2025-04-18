909ce0c32a69dfd3243987d7000d8f809d39d25d
Mikhail Zhilkin <mikhailzhilkin@yandex.ru>
2025-04-18 20:42:15 +0300

    Готовая работа

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,132 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type TProps = {
+	articleState: ArticleStateType;
+	setArticleState: (data: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: TProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleState.contentWidth);
+	const asideRef = useRef<HTMLElement | null>(null);
+
+	const togglePanel = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const hundleOutsideClick = (e: MouseEvent) => {
+		!asideRef.current?.contains(e.target as HTMLElement) && togglePanel();
+	};
+
+	const hundleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		setArticleState({
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		});
+	};
+
+	const hundleReset = () => {
+		setArticleState(defaultArticleState);
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
+
+	useEffect(() => {
+		if (isOpen) {
+			document.addEventListener('mousedown', hundleOutsideClick);
+		} else {
+			document.removeEventListener('mousedown', hundleOutsideClick);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', hundleOutsideClick);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					togglePanel();
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={asideRef}>
+				<form
+					className={styles.form}
+					onSubmit={hundleSubmit}
+					onReset={hundleReset}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={setFontFamily}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						selected={fontSize}
+						options={fontSizeOptions}
+						name='fontSizeBtnGroup'
+						onChange={setFontSize}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={fontColor}
+						options={fontColors}
+						onChange={setFontColor}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={backgroundColor}
+						options={backgroundColors}
+						onChange={setBackgroundColor}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={contentWidth}
+						options={contentWidthArr}
+						onChange={setContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
